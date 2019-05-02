65cb698be9f5fbb42ef8de60c8ce21f72c212b41
tamnntit <nguyenngoctam7997@gmail.com>
2019-05-02 09:21:53 +0700

    sửa lại code cho sạch hơn

---
+++ app/src/main/java/com/example/personal/StatisticsActivity.java
@@ -0,0 +1,13 @@
+package com.example.personal;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class StatisticsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savingInstanceState) {
+        super.onCreate(savingInstanceState);
+        setContentView(R.layout.activity_statistics);
+    }
+
+}
